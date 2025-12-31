73d079b2225d578836628abebb77d45335efec5a
Michal Becmer <michalcloud2003@gmail.com>
2025-12-31 15:26:44 +0000

    🎉 Initial Commit
    Fully working and updated game engine to SFML 3.x

---
+++ GameEngine/gd4_sfml_tcp_game/Player.cpp
@@ -0,0 +1,134 @@
+#include "Player.hpp"
+#include "ReceiverCategories.hpp"
+#include "Aircraft.hpp"
+
+struct AircraftMover
+{
+    AircraftMover(float vx, float vy) :velocity(vx, vy)
+    {}
+    void operator()(Aircraft& aircraft, sf::Time) const
+    {
+        aircraft.Accelerate(velocity);
+    }
+
+    sf::Vector2f velocity;
+};
+
+Player::Player(): m_current_mission_status(MissionStatus::kMissionRunning)
+{
+    //Set initial key bindings
+    m_key_binding[sf::Keyboard::Key::A] = Action::kMoveLeft;
+    m_key_binding[sf::Keyboard::Key::D] = Action::kMoveRight;
+    m_key_binding[sf::Keyboard::Key::W] = Action::kMoveUp;
+    m_key_binding[sf::Keyboard::Key::S] = Action::kMoveDown;
+    m_key_binding[sf::Keyboard::Key::M] = Action::kMissileFire;
+    m_key_binding[sf::Keyboard::Key::Space] = Action::kBulletFire;
+
+    //Set initial action bindings
+    InitialiseActions();
+
+    //Assign all categories to a player's aircraft
+    for (auto& pair : m_action_binding)
+    {
+        pair.second.category = static_cast<unsigned int>(ReceiverCategories::kPlayerAircraft);
+    }
+}
+
+void Player::HandleEvent(const sf::Event& event, CommandQueue& command_queue)
+{
+    if (const auto* keyPressed = event.getIf<sf::Event::KeyPressed>())
+    {
+        auto found = m_key_binding.find(keyPressed->code);
+        if (found != m_key_binding.end() && !IsRealTimeAction(found->second))
+        {
+            command_queue.Push(m_action_binding[found->second]);
+        }
+    }
+}
+
+void Player::HandleRealTimeInput(CommandQueue& command_queue)
+{
+    //Check if any of the key bindings are pressed
+    for (auto pair : m_key_binding)
+    {
+        if (sf::Keyboard::isKeyPressed(pair.first) && IsRealTimeAction(pair.second))
+        {
+            command_queue.Push(m_action_binding[pair.second]);
+        }
+    }
+}
+
+void Player::AssignKey(Action action, sf::Keyboard::Key key)
+{
+    //Remove keys that are currently bound to the action
+    for (auto itr = m_key_binding.begin(); itr != m_key_binding.end();)
+    {
+        if (itr->second == action)
+        {
+            m_key_binding.erase(itr++);
+        }
+        else
+        {
+            ++itr;
+        }
+    }
+    m_key_binding[key] = action;
+}
+
+sf::Keyboard::Key Player::GetAssignedKey(Action action) const
+{
+    for (auto pair : m_key_binding)
+    {
+        if (pair.second == action)
+        {
+            return pair.first;
+        }
+    }
+    return sf::Keyboard::Key::Unknown;
+}
+
+void Player::SetMissionStatus(MissionStatus status)
+{
+    m_current_mission_status = status;
+}
+
+MissionStatus Player::GetMissionStatus() const
+{
+    return m_current_mission_status;
+}
+
+void Player::InitialiseActions()
+{
+    const float kPlayerSpeed = 200.f;
+    m_action_binding[Action::kMoveLeft].action = DerivedAction<Aircraft>(AircraftMover(-kPlayerSpeed, 0.f));
+    m_action_binding[Action::kMoveRight].action = DerivedAction<Aircraft>(AircraftMover(kPlayerSpeed, 0.f));
+    m_action_binding[Action::kMoveUp].action = DerivedAction<Aircraft>(AircraftMover(0.f, -kPlayerSpeed));
+    m_action_binding[Action::kMoveDown].action = DerivedAction<Aircraft>(AircraftMover(0.f, kPlayerSpeed));
+    m_action_binding[Action::kBulletFire].action = DerivedAction<Aircraft>([](Aircraft& a, sf::Time dt)
+        {
+            a.Fire();
+        }
+    );
+
+    m_action_binding[Action::kMissileFire].action = DerivedAction<Aircraft>([](Aircraft& a, sf::Time dt)
+        {
+            a.LaunchMissile();
+        }
+    );
+
+}
+
+bool Player::IsRealTimeAction(Action action)
+{
+    switch (action)
+    {
+    case Action::kMoveLeft:
+    case Action::kMoveRight:
+    case Action::kMoveDown:
+    case Action::kMoveUp:
+    case Action::kBulletFire:
+        return true;
+    default:
+        return false;
+    }
+}
